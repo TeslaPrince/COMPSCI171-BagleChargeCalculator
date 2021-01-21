3ac0c9c184c951142ddc7c56921bf002742567ba
Jenna Lambert <57016635+TeslaPrince@users.noreply.github.com>
2021-01-21 01:51:29 -0600

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub resetButton_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+    End Sub
+    Private Sub calcButton_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        'step 1:declare variables
+        Dim bagels As Integer
+        Dim cost As Double
+        Const price1 As Double = 0.85
+        Const price2 As Double = 0.6
+        'step 2:
+        bagels = CInt(txtBagels.Text)
+        'setp 3: making calculations using if...else
+        If bagels < 10 Then 'if bagels= 0=9
+            cost = price1 * bagels
+            lblPrice.Text = CStr(price1)
+        Else 'if bagels=10+
+            cost = price2 * bagels
+            lblPrice.Text = CStr(price2)
+        End If
+        lblCost.Text = CStr(cost)
+    End Sub
+End Class
